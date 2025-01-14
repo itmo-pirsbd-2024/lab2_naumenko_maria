735552e585232097d8250fca65a41c13be4554fb
Marygith <marianmnk2001@yandex.ru>
2025-01-14 14:48:06 +0300

    added TempDir

--- src/main/java/ru/nms/utils/Constants.java
+++ src/main/java/ru/nms/utils/Constants.java
@@ -1,5 +1,6 @@
 package ru.nms.utils;
 
+import com.google.common.io.Files;
 import org.apache.commons.math3.linear.RealVector;
 
 import java.util.Arrays;
@@ -12,7 +13,7 @@ public class Constants {
     public static final int RADIUS_POSITION = META_DATA_HEADER_SIZE + BALL_TREE_HEADER_SIZE;
     public static final int RADIUS_SIZE = Double.BYTES;
     public static final int CENTROID_POSITION = RADIUS_POSITION + Double.BYTES;
-    public static final String TEMP_DIR = "temp/";
+    public static final String TEMP_DIR = Files.createTempDir().getAbsolutePath();
     public static final Comparator<RealVector> PROJECTION_COMPARATOR = Comparator.comparingDouble(v -> Arrays.stream(v.toArray()).sum());
 
 }
